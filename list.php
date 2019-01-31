72678026ed3cdccaf645aa3a9f98550f68f7338d
Henry <henry@proto366.haagahelia.amk>
2019-01-31 12:35:20 +0200

    muokkasin list-php tyhjäksi

--- list.php
+++ list.php
@@ -1,16 +1,13 @@
-<?php
- if($_POST['password'] == 'password' && $_POST['username'] == 'username') {
- session_start();
- $_SESSION['username']='username';
- redirect("list.php");
- } else {
- echo 'Login failed';
- }
- function redirect($url) {
- ob_start();
- header('Location: '.$url);
- ob_end_flush();
- die();
- }
-?>
-//laureatgrepi
+
+<html>
+ <head>
+ <meta charset="UTF-8">
+ <title>
+ list
+ </title>
+ </head>
+ <body>
+ <h1>List</h1>
+ <table>
+ </body>
+</html>
